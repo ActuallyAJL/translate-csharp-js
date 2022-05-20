4b6c57d3302971d7191e20d11b62951c85a1704c
ActuallyAJL <zero3260@hotmail.com>
2022-05-20 14:19:20 -0500

    did the first two exercises

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,12 +1,25 @@
 // Put your code here
 
 const names = [
-   "Nashville", "Hong Kong", "The back yard", "Earth",
-    "London", "The mall", "Ryman Auditorium", "The Great Wall of China"
+  "Nashville",
+  "Hong Kong",
+  "The back yard",
+  "Earth",
+  "London",
+  "The mall",
+  "Ryman Auditorium",
+  "The Great Wall of China",
 ];
 
 console.log("All Pace Names");
 
 for (const name of names) {
+  console.log(name);
+}
+
+const theNames = names.filter((name) => name.startsWith("The"));
+
+console.log("\n'The' Place Names");
+for (const name of theNames) {
   console.log(name);
 }
